da799709926724955be633423708be357c5edadf
Benjamin Bentmann <benjamin.bentmann@udo.edu>
2011-06-26 15:42:21 +0200

    o Updated to m2e 1.0 and fixed target platform

--- org.sonatype.m2e.subclipse/src/org/sonatype/m2e/subclipse/internal/SubclipseHandler.java
+++ org.sonatype.m2e.subclipse/src/org/sonatype/m2e/subclipse/internal/SubclipseHandler.java
@@ -19,7 +19,7 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
-import org.eclipse.m2e.core.core.IMavenConstants;
+import org.eclipse.m2e.core.internal.IMavenConstants;
 import org.eclipse.m2e.scm.MavenProjectScmInfo;
 import org.eclipse.m2e.scm.spi.ScmHandler;
 
